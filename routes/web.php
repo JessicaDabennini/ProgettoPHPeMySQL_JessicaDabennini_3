c16c047a3dbcda38aeb7c36e17b8c5cbef9b4503
JessicaDabennini <jessica.dabennini@gmail.com>
2026-03-06 11:14:05 +0100

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,9 @@ Route::prefix('api')->group(function () {
     // Rotta per ottenere il totale di CO2 salvato
     Route::get('/total-co2-saved', [ProductController::class, 'getTotalCo2Saved']);
     
-    // Rotta per creare un nuovo prodotto
-    // Route::post('/products', [ProductController::class, 'create']);  
-    
-    // Rotta per eliminare un prodotto
-    // Route::delete('/products', [ProductController::class, 'delete']);
-    
     // Rotta per leggere tutti i prodotti
     Route::get('/products', [ProductController::class, 'read']);
-    
-    // Rotta per aggiornare un prodotto
-    // Route::put('/products/{id}', [ProductController::class, 'update']);
-
-    // Route::post('/orders', [OrderController::class, 'create']);
-    
-    // Route::delete('/orders/{id}', [OrderController::class, 'delete']);
-    
+        
     Route::get('/orders', [OrderController::class, 'read']);
     
-    // Route::put('/orders/{id}', [OrderController::class, 'update']);
 });
